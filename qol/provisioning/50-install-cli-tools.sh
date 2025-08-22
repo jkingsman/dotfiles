23d6412649a0b3df54cd47154dda5f13319dcd3b
Jack Kingsman <jack.kingsman@gmail.com>
2025-08-22 13:52:22 -0700

    No alt c binding

--- qol/provisioning/50-install-cli-tools.sh
+++ qol/provisioning/50-install-cli-tools.sh
@@ -8,6 +8,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Install fzf
 if [ ! -d "${HOME}/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
+    export FZF_ALT_C_COMMAND=""
     "${HOME}/.fzf/install" --key-bindings --no-completion --no-update-rc --no-zsh --no-fish
 fi
 
